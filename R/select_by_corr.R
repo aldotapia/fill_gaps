cbf8f472a531be2ba6631338856767eca278a820
aldotapia <aldo.tapia.a@gmail.com>
2017-12-05 07:56:27 -0300

    Updated select_by_corr

--- R/select_by_corr.R
+++ R/select_by_corr.R
@@ -1,12 +1,14 @@
-select_by_corr <- function(df, aimst, method = 'kendall', datecol = T, only_slope = T, max_st = 3) {
-  if (sum(names(df)[1:3] == c('Main_station','Support_station','Correlation_value')) == 3) {
-    df <- df
-  }else{
-    df <- corr_summary(df, method = method, datecol = datecol, only_slope = only_slope)
-  }
+# agregar seleccionar por fecha
+select_by_corr <- function(df, aimst, method = 'kendall', datecol = T, datecol_pos = 1, only_slope = T, max_st = 3,
+                           start_date = '1970-01', end_date = '2016-12') {
+  df <- cbind.data.frame(df[,datecol_pos], df[,-datecol_pos])
   if (is.numeric(aimst)) {
-    aimst <- unique(as.character(df[,'Main_station'])[aimst])
+    aimst <- names(df)[aimst + 1]
   }
+  # Select data from start_date to end_date where aim station has NA values
+  df <- df[is.na(df[,aimst]) &
+                1:dim(df)[1] %in% which(df[,1] == start_date):which(df[,1] == end_date),]
+  df <- corr_summary(df, method = method, datecol = datecol, only_slope = only_slope)
   temp <- df[df['Main_station'] == aimst,]
   possible_st <- as.character(temp[,'Support_station'])
   corrs <- temp[,'Correlation_value']
